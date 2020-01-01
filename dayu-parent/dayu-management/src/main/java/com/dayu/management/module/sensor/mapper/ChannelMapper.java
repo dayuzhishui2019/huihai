d1fea9d5364d111c69be4671c2e5991271546e97
leus <leus>
2020-01-01 18:50:42 +0800

    [*]增加字段

---
+++ dayu-parent/dayu-management/src/main/java/com/dayu/management/module/sensor/mapper/ChannelMapper.java
@@ -0,0 +1,8 @@
+package com.dayu.management.module.sensor.mapper;
+
+import com.dayu.management.core.Mapper;
+import com.dayu.management.module.sensor.model.ext.Channel;
+
+public interface ChannelMapper extends Mapper<Channel> {
+
+}
