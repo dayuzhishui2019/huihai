26856b1d1910cea96420e7ef972ebff143ee47c3
leus <leus>
2020-01-05 17:01:10 +0800

    [*]修改功能

--- leus-parent/leus-response/src/main/java/com/dayu/response/ExtRunningError.java
+++ leus-parent/leus-response/src/main/java/com/dayu/response/ExtRunningError.java
@@ -1,25 +1,19 @@
 package com.dayu.response;
 
+import lombok.Getter;
+
 public enum ExtRunningError implements RunningError {
 
     STATE_CHECK_ERROR("5001", "运行时检查异常");
 
+    @Getter
     private String code;
 
+    @Getter
     private String message;
 
     ExtRunningError(String code, String message) {
         this.code = code;
         this.message = message;
-    }
-
-    @Override
-    public String getCode() {
-        return null;
-    }
-
-    @Override
-    public String getMessage() {
-        return null;
     }
 }
